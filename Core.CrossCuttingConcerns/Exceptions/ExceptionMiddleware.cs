e6cda3f0102b15984d20d6fd07be92cb8df1c146
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-03-09 20:01:24 +0300

    FileLogging ve MSSQLServerLogging implementasyonu yapıldı.

--- Core.CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
+++ Core.CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
@@ -1,4 +1,6 @@
 ﻿using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Serilog;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
@@ -13,14 +15,19 @@ public class ExceptionMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly HttpExceptionHandler _httpExceptionHandler;
+    private readonly IHttpContextAccessor _contextAccessor;
+    private readonly LoggerServiceBase _loggerService;
 
-    public ExceptionMiddleware(RequestDelegate next)
+    public ExceptionMiddleware(RequestDelegate next,
+        IHttpContextAccessor contextAccessor, LoggerServiceBase loggerService)
     {
         _next = next;
         _httpExceptionHandler = new HttpExceptionHandler();
+        _contextAccessor = contextAccessor;
+        _loggerService = loggerService;
     }
 
-    public async Task Invoke(HttpContext context) 
+    public async Task Invoke(HttpContext context)
     {
         try
         {
@@ -28,8 +35,30 @@ public class ExceptionMiddleware
         }
         catch (Exception exception)
         {
+            await LogException(context, exception);
             await HandleExceptionAsync(context.Response, exception);
         }
+
+    }
+
+    private Task LogException(HttpContext context, Exception exception)
+    {
+        List<LogParameter> logParameters = new()
+        {
+            new LogParameter{Type=context.GetType().Name, Value=exception.ToString()}
+        };
+
+        LogDetailWithException logDetail = new()
+        {
+            ExceptionMessage = exception.Message,
+            MethodName = _next.Method.Name,
+            Parameters = logParameters,
+            User = _contextAccessor.HttpContext?.User.Identity?.Name ?? "?"
+        };
+
+        _loggerService.Error(JsonSerializer.Serialize(logDetail));
+
+        return Task.CompletedTask;
     }
 
     private Task HandleExceptionAsync(HttpResponse response, Exception exception)
---
+++ Core.CrossCuttingConcerns/Serilog/Messages/SerilogMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Serilog.Messages;
+
+public static class SerilogMessages
+{
+    public static string NullOptionsMessage => "Boş bir değer gönderdiniz. Bu durum geçersizdir. Lütfen tekrar deneyiniz.";
+}
